0ce658e1f2f6a5a6826c9bd4beea5e03439c741a
ruben forner <9657174+kingdevil731@users.noreply.github.com>
2020-08-11 20:12:55 +0200

    build

---
+++ frontend/build/precache-manifest.20215a08488e754bef520762dd4d9893.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02105aee92aaad8bac961a033f940582",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b777a2125e54ac327ce3",
+    "url": "/static/css/main.1c26a409.chunk.css"
+  },
+  {
+    "revision": "9a3bae7f5e802e7690f2",
+    "url": "/static/js/2.c3983501.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c3983501.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b777a2125e54ac327ce3",
+    "url": "/static/js/main.c2104414.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "7eea718b48893e461f82049ec46c1e1c",
+    "url": "/static/media/logo.7eea718b.svg"
+  }
+]);
